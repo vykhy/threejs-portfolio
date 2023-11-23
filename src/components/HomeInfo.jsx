7c2cec3085db2e0cc36259408e685f4f54e4b649
vykhy <vikehietoyhoshii@gmail.com>
2023-11-23 07:44:36 +0530

    Home Info Popup

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const renderContent = {
+  1: (
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+      Hi, I am <span className="font-semibold">Vikehieto</span>
+      <br />A Software Engineer from India
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text={
+        "Worked with many companies and picked up many skills along the way"
+      }
+      link={"/about"}
+      btnText={"Learn More"}
+    />
+  ),
+  3: (
+    <InfoBox
+      text={"Developed multiple projects with success. Curious about my work?"}
+      link={"/projects"}
+      btnText={"Visit my portfolio"}
+    />
+  ),
+  4: (
+    <InfoBox
+      text={
+        "Need a project done or looking for a dev? I'm just a few keystrokes away"
+      }
+      link={"/contact"}
+      btnText={"Let's talk"}
+    />
+  ),
+};
+
+function InfoBox({ text, link, btnText }) {
+  return (
+    <div className="info-box">
+      <p className="font-medium sm:text-xl text-center"> {text}</p>
+      <Link to={link} className="neo-brutalism-white neo-btn">
+        {btnText}
+        <img src={arrow} className="w-4 h-4 object-contain" />
+      </Link>
+    </div>
+  );
+}
+
+function HomeInfo({ currentStage }) {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo;
